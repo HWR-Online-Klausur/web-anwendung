3140ebab57fbf3e8bb2643327d51aa79d5ce7947
Ben Schwarz <schwarz_ben@gmx.net>
2021-03-01 11:33:49 +0100

    Parser beta

--- klausur-parser/parser.js
+++ klausur-parser/parser.js
@@ -32,11 +32,8 @@ function aufgabenParse() {
             case 'single-choice':
                 createSingleChoice(a.id, a.fragestellung, a.antworten);
                 break;
-            case 'text-kurz':
-                createTextKurz(a.id, a.fragestellung);
-                break;
-            case 'text-lang':
-                createTextLang(a.id, a.fragestellung);
+            case 'text':
+                createText(a.id, a.fragestellung);
                 break;
             default: break;
         }
@@ -45,26 +42,35 @@ function aufgabenParse() {
 
 // TODO: Create Methoden vollenden
 
-function createMultipleChoice(id, aufgabe, antworten) {
+function createMultipleChoice(id, fragestellung, antworten) {
     let options = "";
+    let i = 0;
     for (const a of antworten) {
-        options += `<option value="${a}">${a}</option>`;
+        const tempID = `${id}-${i}`;
+        options += `<input type="checkbox" id="${tempID}" name="${tempID}" value="${a}"><label for="${tempID}">${fragestellung}</label>`;
+        i++;
     }
-    const html = `<div id="${id}-div"><p>${aufgabe}</p></div>`;
+    const html = `<div id="${id}-div"><p>${fragestellung}</p>${options}</div>`;
 
     klausur.addKlausurHTML(html);
 }
 
-function createSingleChoice(id, aufgabe, antworten) {
+function createSingleChoice(id, fragestellung, antworten) {
+    let options = "";
+    let i = 0;
+    for (const a of antworten) {
+        const tempID = `${id}-${i}`;
+        options += `<input type="radio" id="${tempID}" name="${id}" value="${a}"><label for="${tempID}">${a}</label>`;
+        i++;
+    }
+    const html = `<div id="${id}-div"><p>${fragestellung}</p>${options}</div>`;
 
+    klausur.addKlausurHTML(html);
 }
 
-function createTextKurz(id, aufgabe) {
-
-}
-
-function createTextLang(id, aufgabe) {
-
+function createText(id, fragestellung) {
+    const html = `<div id="${id}-div"><p>${fragestellung}</p><textarea name="${id}"></textarea></div>`;
+    klausur.addKlausurHTML(html);
 }
 
 aufgabenParse();

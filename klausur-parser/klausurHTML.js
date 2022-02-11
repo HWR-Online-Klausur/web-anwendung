bf1da1301f6e7f5550d7b565b4ecac6e9b274811
Artem Suttar <artem.suttar@gmail.com>
2022-02-11 12:44:59 +0100

    Add parser.test.js and KlausurHTML:aufgabenParse
    Add 3 Test Cases for 3 parser.js functions, add Test Case for
    aufgabenParse at klausurHTML.test.js

--- klausur-parser/klausurHTML.js
+++ klausur-parser/klausurHTML.js
@@ -19,22 +19,25 @@ class KlausurHTML{
 
     aufgabenParse(klausur) {
         this.clearKlausurHTML();
-        for (const a of klausur) {
-            switch (a.methode) {
-                case 'multiple-choice':
-                    this.addKlausurHTML(createMultipleChoice(a.id, a.fragestellung, a.antworten));
-                    break;
-                case 'single-choice':
-                    this.addKlausurHTML(createSingleChoice(a.id, a.fragestellung, a.antworten));
-                    break;
-                case 'text':
-                    this.addKlausurHTML(createText(a.id, a.fragestellung));
-                    break;
-                default: break;
+        try {
+            for (const a of klausur) {
+                switch (a.methode) {
+                    case 'multiple-choice':
+                        this.addKlausurHTML(createMultipleChoice(a.id, a.fragestellung, a.antworten));
+                        break;
+                    case 'single-choice':
+                        this.addKlausurHTML(createSingleChoice(a.id, a.fragestellung, a.antworten));
+                        break;
+                    case 'text':
+                        this.addKlausurHTML(createText(a.id, a.fragestellung));
+                        break;
+                    default: break;
+                }
             }
+        }catch (e){
+            //TODO:Do something if the data is not valid
         }
     }
-
 }
 
 module.exports = new KlausurHTML();
--- klausur-parser/klausurHTML.test.js
+++ klausur-parser/klausurHTML.test.js
@@ -41,3 +41,55 @@ describe('klausurHTML: clearKlausurHTML',() => {
         })
     });
 })
+
+describe('klausurHTML: aufgabenParse',() => {
+    let obj
+    obj = {
+        dozent: "Dr. Dev",
+        modul: "IT-1234",
+        aufgaben: [
+            {
+                "fragestellung": "Welche dieser Zahlen ist größer als 3?",
+                "methode": "multiple-choice",
+                "antworten": ["1", "3", "5", "-12"],
+                "id": "ab1"
+            },
+            {
+                "fragestellung": "Schreiben Sie drei Wörter",
+                "methode": "text",
+                "antworten": [],
+                "id": "ab2"
+            },
+            {
+                "fragestellung": "Sind Sie ein Roboter?",
+                "methode": "single-choice",
+                "antworten": ["Ja", "Nein"],
+                "id": "ab4"
+            }
+        ]
+    }
+    test('the attribut KlausurHTML should be empty if we use non valid data', () =>{
+        klausurHTML.aufgabenParse(obj);
+        assert.deepEqual(klausurHTML,{
+            KlausurHTML: []
+        })
+    });
+
+    test('the attribut KlausurHTML should not be empty if we use a valid data', () =>{
+        klausurHTML.aufgabenParse(obj.aufgaben);
+        assert.notDeepEqual(klausurHTML,{
+            KlausurHTML: []
+        })
+    });
+
+    test('the attribut KlausurHTML should not be empty and equal to the result (see Code)', () =>{
+        const result = [
+            '<div id="ab1-div"><p>Welche dieser Zahlen ist größer als 3?</p><input class="form-check-input" type="checkbox" id="ab1-0" name="ab1-0" value="1"><label class="form-check-label" for="ab1-0">1</label> <br><input class="form-check-input" type="checkbox" id="ab1-1" name="ab1-1" value="3"><label class="form-check-label" for="ab1-1">3</label> <br><input class="form-check-input" type="checkbox" id="ab1-2" name="ab1-2" value="5"><label class="form-check-label" for="ab1-2">5</label> <br><input class="form-check-input" type="checkbox" id="ab1-3" name="ab1-3" value="-12"><label class="form-check-label" for="ab1-3">-12</label> <br></div><hr>',
+            '<div id="ab2-div"><p>Schreiben Sie drei Wörter</p><textarea class="form-control" name="ab2"></textarea></div><hr>',
+            '<div id="ab4-div"><p>Sind Sie ein Roboter?</p><input class="form-check-input" type="radio" id="ab4-0" name="ab4" value="Ja"><label class="form-check-label" for="ab4-0">Ja</label> <br><input class="form-check-input" type="radio" id="ab4-1" name="ab4" value="Nein"><label class="form-check-label" for="ab4-1">Nein</label> <br></div><hr>'
+        ]
+
+        klausurHTML.aufgabenParse(obj.aufgaben);
+        expect(klausurHTML.getKlausurHTML()).toEqual(result)
+    });
+})

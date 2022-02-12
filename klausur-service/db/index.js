bac093d517864d41a2b1e95e9831efd35aaffecf
B-Schwarz <29834059+B-Schwarz@users.noreply.github.com>
2022-02-12 12:14:38 +0100

    Fix: DB URI

--- klausur-service/db/index.js
+++ klausur-service/db/index.js
@@ -3,17 +3,21 @@ const mongoose = require('mongoose');
 
 //Create connection
 const connectDB = async() => {
-    await mongoose.connect('mongodb://localhost:27017/onlineKlausur', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false
-    }).then(()=>{
-        console.log("Erfolgreiche Datenbankverbindung");
-    }).catch((e) =>{
-        console.log("Fehler beim Verbinden mit der Datenbank");
-        console.log(e);
-    });
+    if (process.env.DB_URI === undefined || process.env.DB_URI === '') {
+        throw "DB_URI NOT SET"
+    } else {
+        await mongoose.connect(process.env.DB_URI + '/onlineKlausur', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useFindAndModify: false
+        }).then(() => {
+            console.log("Erfolgreiche Datenbankverbindung");
+        }).catch((e) => {
+            console.log("Fehler beim Verbinden mit der Datenbank");
+            console.log(e);
+        });
+    }
 }
 
 module.exports = {

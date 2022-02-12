23d6b7f1e30918c37d6fedc423a60608ff214254
B-Schwarz <29834059+B-Schwarz@users.noreply.github.com>
2022-02-12 19:35:26 +0100

    Add session

--- klausur-service/index.js
+++ klausur-service/index.js
@@ -7,13 +7,37 @@ const {connectDB} = require('./db');
 const router = require('./routes');
 const errorHandler = require('./middleware/errorHandlingMiddleware');
 
+const session = require('express-session')
+const MongoStore = require('connect-mongo')
+
 //region Middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+
+const store = MongoStore.create({
+    mongoUrl: process.env.DB_URI,
+    collectionName: 'sessions'
+})
+
+const sess = session({
+    name: 'onlineklausur.sid',
+    secret: 'safesecret',
+    saveUninitialized: true,
+    resave: true,
+    store: store,
+    cookie: {
+        httpOnly: true,
+        maxAge: 999999999999,
+        sameSite: 'lax',
+        secure: false
+    }
+})
+
+app.use(sess)
+
 app.use(express.static(__dirname + "/static"));
 app.use('/api', router);
 //endregion Middleware
-
 
 //Error handler. Should always be last middleware!
 app.use(errorHandler);
